87ce35d726282362276e31830de952e03fa85e1e
Tobias Strauß <tobias.strauss@planet.de>
2019-01-28 09:00:21 +0100

    create bin-LM-files

--- src/main/java/de/uros/citlab/module/workflow/RunTrainingLM.java
+++ src/main/java/de/uros/citlab/module/workflow/RunTrainingLM.java
@@ -2,6 +2,7 @@ package de.uros.citlab.module.workflow;
 
 import com.achteck.misc.exception.InvalidParameterException;
 import de.planet.languagemodel.train.TrainLM;
+import edu.berkeley.nlp.lm.io.MakeLmBinaryFromArpa;
 import de.uros.citlab.module.util.FileUtil;
 import de.uros.citlab.module.util.PageXmlUtil;
 
@@ -20,7 +21,10 @@ public class RunTrainingLM {
         for (File file : xml) {
             text.addAll(PageXmlUtil.getText(PageXmlUtil.unmarshal(file)));
         }
-        TrainLM.train(text, 5, out);
+        File arpa=new File(out.getAbsolutePath().replace(".bin",".arpa"));
+        TrainLM.train(text, 5, arpa);
+        MakeLmBinaryFromArpa.main(new String[]{arpa.getAbsolutePath(),out.getAbsolutePath()});
+
     }
 
     public static void main(String[] args) throws InvalidParameterException {
@@ -28,6 +32,6 @@ public class RunTrainingLM {
         RunTrainingLM training = new RunTrainingLM();
         training.run(
                 new File("/home/gundram/devel/projects/read/data/TRAIN_CITlab_Bentham_himself_M4"),
-                new File("/home/gundram/devel/projects/read/langmods/M4.arpa"));
+                new File("/home/gundram/devel/projects/read/langmods/M4.bin"));
     }
 }
